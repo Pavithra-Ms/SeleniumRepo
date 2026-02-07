d31e48174c832fe348c858583364bac221d79568
Pavithra <67957044+Pavithra-Ms@users.noreply.github.com>
2026-02-07 22:13:27 +0530

    Add files via upload

---
+++ Week2/day1Assignments/CreateAccount.java
@@ -0,0 +1,26 @@
+package Week2.day1Assignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateAccount {
+			public static void main(String[] args) {
+				ChromeDriver driver= new ChromeDriver();
+				driver.manage().window().maximize();
+				driver.get("http://leaftaps.com/opentaps/control/main");
+				driver.findElement(By.id("username")).sendKeys("democsr2");
+				driver.findElement(By.id("password")).sendKeys("crmsfa");
+				driver.findElement(By.className("decorativeSubmit")).click();
+				driver.findElement(By.partialLinkText("CRM")).click();
+				driver.findElement(By.linkText("Accounts")).click();//Navigate to the accounts tab
+				driver.findElement(By.linkText("Create Account")).click();//To navigate the create account
+				driver.findElement(By.id("accountName")).sendKeys("Dora");//Account name
+				driver.findElement(By.id("numberEmployees")).sendKeys("1750305");//Employee number
+				driver.findElement(By.id("officeSiteName")).sendKeys("LeafTaps");//Site name 
+				driver.findElement(By.className("smallSubmit")).click();
+				String title = driver.getTitle();
+				System.out.println(title);	// to print the title of the web-site
+				driver.close();//To force close the browser
+			}
+			
+}
---
+++ week2_day1/FacebookLocators.java
@@ -0,0 +1,19 @@
+package week2_day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookLocators {
+		public static void main(String[] args) {
+			ChromeDriver driver=new ChromeDriver(); 
+			driver.manage().window().maximize()	;
+			driver.get("http://facebook.com"); 
+			driver.findElement(By.id("email")).sendKeys("testleaf.2023@gmail.com");
+			driver.findElement(By.id("pass")).sendKeys("Tuna@321");
+			driver.findElement(By.name("login")).click();
+			String title = driver.getTitle();
+			System.out.println(title);	
+			driver.close();//--used to close the browser
+			}
+
+	}
